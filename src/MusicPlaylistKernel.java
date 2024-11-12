5e768b1f306a1d4452f70a4eb132da8e78c25e6b
Nijaa Nishanth <nijaa.nishanth@gmail.com>
2024-11-11 20:37:05 -0500

    changed interfaces

--- src/MusicPlaylistKernel.java
+++ src/MusicPlaylistKernel.java
@@ -1,55 +1,35 @@
 import components.map.Map;
-import components.queue.Queue;
 import components.standard.Standard;
 
-/*
- * Kernel Implementation for {@code MusicPlaylist} implemented on {@code Queue} with {@code Map}
+/**
+ * Kernel Implementation for {@code MusicPlaylist} implemented on {@code Queue}
+ * with {@code Map}
  *
- * @param Queue<Map<String, String>>
  * @author Nijaa Nishanth
  */
 public interface MusicPlaylistKernel extends Standard<MusicPlaylist> {
 
-    /* Queue to add songs */
-    private Queue<Map<String, String>> playlist;
-
-    /* song */
-    private Map<String, String> song;
-
-    /*
-     * Creator of initial representation.
-     *
-     * @ensures this.playlist = new playlist
-     */
-    private void createNewRep();
-
-    /*
-     * No-argument constructor
-     */
-    public MusicPlaylistKernel();
-
-    /*
-     * Kernel Methods
-     */
-
-    /*
+    /**
      * Adds a song to the end of the playlist.
      *
-     * @param a the name of the artist of the song to be added
+     * @param a
+     *            the name of the artist of the song to be added
      *
-     * @param n the name of the song to be added
+     * @param n
+     *            the name of the song to be added
      *
      * @updates: this
      *
      * @ensures: this = #this * n
      *
      */
-    public final void addSong(String n, String a);
+    void addSong(String n, String a);
 
-    /*
+    /**
      * Removes a song from the playlist.
      *
-     * @param n the name of the song to be removed
+     * @param n
+     *            the name of the song to be removed
      *
      * @updates: this
      *
@@ -60,15 +40,15 @@ public interface MusicPlaylistKernel extends Standard<MusicPlaylist> {
      * @ensures: this = <removed> * #this
      *
      */
-    public final Map<String, String> removeSong(String n);
+    Map<String, String> removeSong(String n);
 
-    /*
+    /**
      * Returns the length of the playlist.
      *
      * @returns: number of songs in the playlist
      *
      * @ensures: length = |this|
      */
-    private final int length();
+    int length();
 
 }

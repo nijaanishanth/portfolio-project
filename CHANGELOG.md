3b6e439bb1525e5db00622eaa4bd2b69fb3de54f
Nijaa Nishanth <nijaa.nishanth@gmail.com>
2024-11-11 21:36:52 -0500

    implemented abstract class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,15 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Calendar Versioning](https://calver.org/) of
 the following form: YYYY.0M.0D.
 
+## 2024.11.11
+
+### Added
+
+- Implemented abstract class
+- Implemented shuffle()
+- Implemented addToQueue()
+- Implemented playFromSong()
+
 ## 2024.09.16
 
 ### Added
---
+++ src/MusicPlaylistKernel.java
@@ -0,0 +1,55 @@
+
+import components.map.Map;
+import components.standard.Standard;
+
+/**
+ * Kernel Implementation for {@code MusicPlaylist} implemented on {@code Queue}
+ * with {@code Map}.
+ *
+ * @author Nijaa Nishanth
+ */
+public interface MusicPlaylistKernel extends Standard<MusicPlaylist> {
+
+    /**
+     * Adds a song to the end of the playlist.
+     *
+     * @param a
+     *            the name of the artist of the song to be added
+     *
+     * @param n
+     *            the name of the song to be added
+     *
+     * @updates this
+     *
+     * @ensures this = #this * n
+     *
+     */
+    void addSong(String n, String a);
+
+    /**
+     * Removes a song from the playlist.
+     *
+     * @param n
+     *            the name of the song to be removed
+     *
+     * @updates this
+     *
+     * @return the song name and artist of the removed song
+     *
+     * @requires this/= <>
+     *
+     * @ensures this = <removed> * #this
+     *
+     */
+    Map<String, String> removeSong(String n);
+
+    /**
+     * Returns the length of the playlist.
+     *
+     * @return number of songs in the playlist
+     *
+     * @ensures length = |this|
+     */
+    int length();
+
+}

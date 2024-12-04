6af07d30e689dba50723a5b06a7b8461a377492e
Nijaa Nishanth <nijaa.nishanth@gmail.com>
2024-12-04 03:53:15 -0500

    added kernel test cases

---
+++ test/MusicPlaylist1Test.java
@@ -0,0 +1,32 @@
+/**
+ * JUnit test fixture for {@code MusicPlaylist}'s secondary methods.
+ *
+ * @author Nijaa Nishanth
+ */
+public class MusicPlaylist1Test {
+
+    /**
+     * toString test cases.
+     */
+
+    /**
+     * hashCode test cases.
+     */
+
+    /**
+     * equals test cases.
+     */
+
+    /**
+     * shuffle test cases.
+     */
+
+    /**
+     * addToQueue test cases.
+     */
+
+    /**
+     * playFromSong test cases.
+     */
+
+}
